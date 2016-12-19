22567f23a08bacbff26cbd079f280ccb95e9cee6
ashikul <alam.ashikul@gmail.com>
2016-12-18 23:30:38 -0500

    add in sign and hello sign quick start

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var sign = require('./routes/sign');
 
 var app = express();
 
@@ -22,8 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ROUTES HERE
 app.use('/', index);
 app.use('/users', users);
+app.use('/sign', sign);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/sign.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var hellosign = require('hellosign-sdk')({key: '390ccd769ca10e086147c642b4be62d8a4b60de2b8db089cfdd30bf65d80671f'});
+
+var options = {
+    test_mode : 1,
+    clientId : 'YOUR_clientId',
+    subject : 'My First embedded signature request',
+    message : 'Awesome, right?',
+    signers : [
+        {
+            email_address : 'alam.ashikul@gmail.com',
+            name : 'Ashikul Alam'
+        }
+    ],
+    files : ['NDA.pdf']
+};
+
+hellosign.signatureRequest.createEmbedded(options)
+    .then(function(response){
+        //parse response
+    })
+    .catch(function(err){
+        //catch error
+    });
+
+
+/* GET sign listing. */
+router.get('/', function (req, res, next) {
+    res.send({
+        data: 'hey123',
+        test: true
+    });
+});
+
+router.post('/', function (req, res, next) {
+    res.send({
+        data: 'hey123',
+        test: true
+    });
+});
+
+module.exports = router;
