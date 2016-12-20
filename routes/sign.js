0f31cda005d08b87608370c3d87c34babb3a5bb6
ashikul <alam.ashikul@gmail.com>
2016-12-19 21:47:50 -0500

    man I had to rename prop, error in documentation..

--- routes/sign.js
+++ routes/sign.js
@@ -3,34 +3,77 @@ var router = express.Router();
 var hellosign = require('hellosign-sdk')({key: '390ccd769ca10e086147c642b4be62d8a4b60de2b8db089cfdd30bf65d80671f'});
 
 var options = {
-    test_mode : 1,
-    clientId : 'YOUR_clientId',
-    subject : 'My First embedded signature request',
-    message : 'Awesome, right?',
-    signers : [
+    test_mode: 1,
+    client_id: 'd6549f6f55de34ef02b0b9004a97272b',
+
+    // template_id : 'c0b0d6d496d11b1b365deb403580feec',
+    subject: 'My First embedded signature request with a reusable form',
+    message: 'Awesome, right?',
+    signers: [
         {
-            email_address : 'alam.ashikul@gmail.com',
-            name : 'Ashikul Alam'
+            email_address: 'jack@example.com',
+            name: 'Jack',
+            role: 'Manager'
+        },
+        {
+            email_address: 'jill@example.com',
+            name: 'Jill',
+            role: 'Employee'
         }
     ],
-    files : ['NDA.pdf']
+    files: ['https://firebasestorage.googleapis.com/v0/b/notary-fb8b1.appspot.com/o/edinfo-sample-forms.pdf?alt=media&token=9ca9f31a-5707-45b0-b474-17adc5e8d0b1.pdf']
 };
-
-hellosign.signatureRequest.createEmbedded(options)
-    .then(function(response){
-        //parse response
-    })
-    .catch(function(err){
-        //catch error
-    });
-
 
 /* GET sign listing. */
 router.get('/', function (req, res, next) {
     res.send({
-        data: 'hey123',
+        data: 'hey12323',
         test: true
     });
+});
+
+router.get('/hello', function (req, res, next) {
+
+    var result;
+    console.log('HELLO!');
+    console.log(hellosign.signatureRequest.createEmbedded(options));
+
+    hellosign.signatureRequest.createEmbedded(options)
+        .then(function (response) {
+            var signatureId = response.signature_request.signatures[0].signature_id;
+            return hellosign.embedded.getSignUrl(signatureId);
+        })
+        .then(function (response) {
+            console.log('URL = ' + response.embedded.sign_url);
+            result = response.embedded.sign_url;
+        })
+        .catch(function (err) {
+            //catch error
+            console.log('ERROR');
+            console.log(err);
+        });
+
+    res.send(result);
+
+    // //TODO: this crap aint workgin!!!
+    // return hellosign.signatureRequest.createEmbedded(options)
+    //     .then(function (response) {
+    //         //parse response
+    //         console.log('testing signatureRequest');
+    //         console.log(response);
+    //         var signatureId = response.signature_request.signatures[0].signature_id;
+    //         return hellosign.embedded.getSignUrl(signatureId);
+    //     })
+    //     .then(function (response) {
+    //         console.log('URL = ' + response.embedded.sign_url);
+    //         result = response.embedded.sign_url;
+    //         res.send(result);
+    //     })
+    //     .catch(function (err) {
+    //         //catch error
+    //         res.send(error);
+    //     });
+
 });
 
 router.post('/', function (req, res, next) {
