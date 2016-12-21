af0ce34e2c5560cea45ef54c4f0219e242eb2f03
ashikul <alam.ashikul@gmail.com>
2016-12-20 21:35:53 -0500

    ok this works i get https://www.hellosign.com/editor/embeddedSign?signature_id=e1a5f0c13eb99e17547c7c9f8becc80e&token=f788f047394d76c285b307bb6f7079b2 as a response

--- routes/sign.js
+++ routes/sign.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var hellosign = require('hellosign-sdk')({key: '390ccd769ca10e086147c642b4be62d8a4b60de2b8db089cfdd30bf65d80671f'});
+var hellosign = require('hellosign-sdk')({
+    key: '390ccd769ca10e086147c642b4be62d8a4b60de2b8db089cfdd30bf65d80671f',
+    client_id: 'd6549f6f55de34ef02b0b9004a97272b'
+});
+// var http = require('http');
+
+// var ndapdf = require('./NDA.pdf');
+
+// var ndapdf = express.static(__dirname + './NDA.pdf');
+
+// var ndapdf = request('https://firebasestorage.googleapis.com/v0/b/notary-fb8b1.appspot.com/o/edinfo-sample-forms.pdf?alt=media&token=9ca9f31a-5707-45b0-b474-17adc5e8d0b1.pdf');
+
+// var ndapdf = http.get("http://firebasestorage.googleapis.com/v0/b/notary-fb8b1.appspot.com/o/edinfo-sample-forms.pdf?alt=media&token=9ca9f31a-5707-45b0-b474-17adc5e8d0b1.pdf", function(response) {
+//     response.pipe(file);
+// });
+
+// var ndapdf = '../assets/NDA.pdf';
+var ndapdf = process.cwd() +  "/assets/NDA.pdf";
 
 var options = {
     test_mode: 1,
-    client_id: 'd6549f6f55de34ef02b0b9004a97272b',
+    // client_id: 'd6549f6f55de34ef02b0b9004a97272b',
+    clientId: 'd6549f6f55de34ef02b0b9004a97272b',
 
     // template_id : 'c0b0d6d496d11b1b365deb403580feec',
-    subject: 'My First embedded signature request with a reusable form',
-    message: 'Awesome, right?',
+    subject: '1My First embedded signature request with a reusable form',
+    message: '1Awesome, right?',
     signers: [
         {
             email_address: 'jack@example.com',
@@ -21,7 +39,8 @@ var options = {
             role: 'Employee'
         }
     ],
-    files: ['https://firebasestorage.googleapis.com/v0/b/notary-fb8b1.appspot.com/o/edinfo-sample-forms.pdf?alt=media&token=9ca9f31a-5707-45b0-b474-17adc5e8d0b1.pdf']
+    // files: ['NDA.pdf']
+    files: ['assets/NDA.pdf']
 };
 
 /* GET sign listing. */
@@ -36,7 +55,8 @@ router.get('/hello', function (req, res, next) {
 
     var result;
     console.log('HELLO!');
-    console.log(hellosign.signatureRequest.createEmbedded(options));
+    console.log(ndapdf);
+    // console.log(hellosign.signatureRequest.createEmbedded(options));
 
     hellosign.signatureRequest.createEmbedded(options)
         .then(function (response) {
